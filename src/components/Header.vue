08e8623f17e158ffb8133c5bd55c34d45d740a7c
Christopher Smith <christophersmith@Christophers-MacBook-Pro.local>
2019-02-04 23:22:50 -0600

    Getting started with the layout, may include the fullstack in this application.

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <header>
+        <img class="Logo" src="../assets/CryptoLogo2.svg" />
+    </header>
+</template>
+
+<script>
+    export default{
+        name: 'Header'
+    }
+</script>
+
+<style scoped>
+    header{
+    }
+    .Logo{
+    }
+</style>
---
+++ src/components/MainSite.vue
@@ -0,0 +1,19 @@
+<template>
+  <Header />
+</template>
+
+<script>
+import Header from './Header.vue'
+//import MainSite from './components/MainSite.vue'
+//import MainSite from './components/MainSite.vue'
+
+export default {
+  name: "MainSite",
+  components:{
+    Header
+  }
+}
+</script>
+
+<style scoped>
+</style>

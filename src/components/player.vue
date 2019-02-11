d784814b2506ee79be008b59a8259deccdfe5b86
Christopher Smith <christophersmith@Christophers-MacBook-Pro.local>
2019-02-10 22:41:35 -0600

    Initializing new branch. Have created a button that can play audio when pressed.

--- src/components/player.vue
+++ src/components/player.vue
@@ -1,58 +1,25 @@
 <template>
     <div>
-        <h3>5G</h3>
-        <audio controls controlsList="nodownload">
-            <source src="../assets/tracks/5G.mp3" type="audio/mpeg">
-        </audio>
-        <h3>VPN</h3>
-        <audio controls controlsList="nodownload">
-            <source src="../assets/tracks/5G.mp3" type="audio/mpeg">
-        </audio>
-        <h3>3 A.M.</h3>
-        <audio controls controlsList="nodownload">
-            <source src="../assets/tracks/5G.mp3" type="audio/mpeg">
-        </audio>
-        <h3>Amazon Prime</h3>
-        <audio controls controlsList="nodownload">
-            <source src="../assets/tracks/5G.mp3" type="audio/mpeg">
-        </audio>
-        <h3>OMW</h3>
-        <audio controls controlsList="nodownload">
-            <source src="../assets/tracks/5G.mp3" type="audio/mpeg">
-        </audio>
-        <h3>Peach Emoji (Interlude)</h3>
-        <audio controls controlsList="nodownload">
-            <source src="../assets/tracks/5G.mp3" type="audio/mpeg">
-        </audio>
-        <h3>Torrents</h3>
-        <audio controls controlsList="nodownload">
-            <source src="../assets/tracks/5G.mp3" type="audio/mpeg">
-        </audio>
-        <h3>Relapse</h3>
-        <audio controls controlsList="nodownload">
-            <source src="../assets/tracks/5G.mp3" type="audio/mpeg">
-        </audio>
-        <h3>Withdrawal</h3>
-        <audio controls controlsList="nodownload">
-            <source src="../assets/tracks/5G.mp3" type="audio/mpeg">
-        </audio>
-        <h3>Sober</h3>
-        <audio controls controlsList="nodownload">
-            <source src="../assets/tracks/5G.mp3" type="audio/mpeg">
-        </audio>
-        <h3>A New Day</h3>
-        <audio controls controlsList="nodownload">
-            <source src="../assets/tracks/5G.mp3" type="audio/mpeg">
-        </audio>
+        <button v-on:click="play"></button>
     </div>
 </template>
 
 <script>
+var song = new Audio('../assets/tracks/5G.mp3');
 export default{
-    name: "player"
+    name: "player",
+    methods:{
+        play: function (){
+            song.play();
+        }
+    }
 }
 </script>
 
 <style scoped>
-
+button{
+    width:30px;
+    height:30px; 
+    background-color: white;
+}
 </style>
